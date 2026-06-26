f20d27751bffab90d24f63980a9b149baef8d490
VitorVT9 <vsilvab57@gmail.com>
2026-06-26 00:08:44 -0300

    feat: adiciona chamada de criação do novo arquivo limpo

---
+++ src/main.rb
@@ -0,0 +1,15 @@
+require_relative 'siasus_etl'
+
+# 1. Avisa onde estão os arquivos
+dir_atual = __dir__
+arquivos_brutos = [
+  File.join(dir_atual, "../Dadosbrutos/PARS2510a.csv"),
+  File.join(dir_atual, "../Dadosbrutos/PARS2510b.csv"),
+  File.join(dir_atual, "../Dadosbrutos/PARS2511.csv"),
+  File.join(dir_atual, "../Dadosbrutos/PARS2512.csv")
+]
+caminho_saida = File.join(dir_atual, "../base_idosos_tratada.csv")
+
+# 2. Executa a função
+puts "Iniciando a extração dos dados..."
+limpar_dados_siasus(arquivos_brutos, caminho_saida)
